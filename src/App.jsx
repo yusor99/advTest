a169d9ecabdeb6dd593b006ba86cb05e18749b30
yusor99 <yusorahmad@gmail.com>
2021-08-15 00:06:43 +0300

    homeadv componnent

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AdvertisementPage from './components/advPage/advertisementPage';
+import './styles/App.css';
+
+function App() {
+  return (
+    <AdvertisementPage />
+  );
+}
+
+export default App;
---
+++ src/components/advPage/advertisementPage.jsx
@@ -0,0 +1,10 @@
+import HomeAdv from "../homeAdv"
+
+const AdvertisementPage = () => {
+    return (
+        <div className="container">
+            <HomeAdv />
+        </div>
+    )
+}
+export default AdvertisementPage;
